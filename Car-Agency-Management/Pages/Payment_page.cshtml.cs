53d0fffde50cc438ed6dadafa89435ef2e374d41
Amr hamed <hamr04180@gmail.com>
2025-12-04 18:14:59 +0200

    car gallary

---
+++ Car-Agency-Management/Pages/Payment_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Agency_Management.Pages
+{
+    public class Payment_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
