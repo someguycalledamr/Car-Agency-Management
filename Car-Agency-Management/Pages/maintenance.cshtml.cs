350223fea8d89fb732875ff128c43ba64cd42d94
HP <HP@BODY>
2025-12-06 15:37:34 +0200

    mainenance

---
+++ Car-Agency-Management/Pages/maintenance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Agency_Management.Pages
+{
+    public class maintenanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
